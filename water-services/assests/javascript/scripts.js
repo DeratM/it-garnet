880ddb0e631b28c3c2cde3a4df9fef51927e7754
DeratM <143150184+DeratM@users.noreply.github.com>
2024-04-01 17:45:22 +0000

    water level update

---
+++ water-services/assests/javascript/scripts.js
@@ -0,0 +1,31 @@
+// Date Function
+    function myFunction() {
+        var FromDate;
+        if (document.getElementById("boxley").checked) {
+            document.getElementById("FromDate").value="1993-04-17";
+        }
+        if (document.getElementById("carver").checked) {
+            document.getElementById("FromDate").value= "2012-09-20";
+        }
+        if (document.getElementById("ponca").checked) {
+            document.getElementById("FromDate").value= "2008-05-03";
+        }
+        if (document.getElementById("pruitt").checked) {
+            document.getElementById("FromDate").value= "2008-06-14";
+        }
+    }
+
+
+// Graph Display Function
+
+
+
+
+
+
+
+
+// Clear Everthing Function
+  function clearform() {
+    document.getElementById("riverError").innerHTML = "";
+  }
