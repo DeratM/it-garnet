1184add3d0eae228e9236959f887d25f15167193
DeratM <143150184+DeratM@users.noreply.github.com>
2024-04-04 18:38:58 +0000

    water levels v2

--- water-services/assests/javascript/scripts.js
+++ water-services/assests/javascript/scripts.js
@@ -1,17 +1,34 @@
-async function getData() {
-        
- // URL and method used with AJAX Call
+async function GetData() {  
     var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=07055680,07055646,07055780,07055660&indent=on&period=P7D&siteStatus=all&parameterCd=00065";
+    var msgObject = await fetch(myURL);
+    var msgJSONText = await msgObject.text();
+    var msg = JSON.parse(msgJSONText);  
     
-// Get the info
-    let myCalcObject = await fetch(myURL);
-    let myResult = await myCalcObject.text();
-       
-// Show the results
-    document.getElementById("results").innerHTML = myResult;
-    }
 
-// Clear Button
-    function clearForm() {
-        document.getElementById("results").innerHTML = "";
+    var dates = [];
+    var values = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[0].values[0].value.length
+    for (var i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[0].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime
     }
+    var sitename = msg.value.timeSeries[0].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[0].variable.variableDescription
+
+    let ctx0 = document.getElementById("chartjs-0");
+    var myChart = new Chart(ctx0, {
+        type: "line",
+        data: {
+            labels: dates,
+            datasets: [{"dates":"values",
+                label: "values", 
+                data: values,
+                fill: false,
+                borderColor: "rgb(75, 192, 192)",
+                lineTension: 0.1
+            }]
+        }
+    });
+}
