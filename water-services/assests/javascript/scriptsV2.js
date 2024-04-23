11e3ea66cfe2e84c41a9fb17f998f5c554ba1ebc
DeratM <143150184+DeratM@users.noreply.github.com>
2024-04-23 16:16:20 +0000

    Water Level fix

--- water-services/assests/javascript/scriptsV2.js
+++ water-services/assests/javascript/scriptsV2.js
@@ -1,23 +1,27 @@
-async function GetData() {  
-    var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=07055680,07055646,07055780,07055660&indent=on&period=P7D&siteStatus=all&parameterCd=00065";
+async function calculate() {
+    /* URL for AJAX Call */
+    var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=07055660,%2007055646,%2007055780,%2007055680&period=P7D&parameterCd=00065&siteStatus=all"
+    /* Make the AJAX call */
     var msgObject = await fetch(myURL);
+    /* Check the status */
     var msgJSONText = await msgObject.text();
-    var msg = JSON.parse(msgJSONText);  
     
-
+    var msg = JSON.parse(msgJSONText);
+    
+    /* Site 0 */
     var dates = [];
     var values = [];
     /* fLen contains the length of the array (number of values) */
     fLen = msg.value.timeSeries[0].values[0].value.length
-    for (var i = 0; i < fLen; i++) {
+    for (i = 0; i < fLen; i++) {
         values[i] = msg.value.timeSeries[0].values[0].value[i].value
         dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime
     }
     var sitename = msg.value.timeSeries[0].sourceInfo.siteName
     var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value
     var siteDescription = msg.value.timeSeries[0].variable.variableDescription
-
-    let ctx = document.getElementById("chartjs-0");
+    
+    var ctx = document.getElementById("chartjs-0");
     var myChart = new Chart(ctx, {
         "type":"line",
         "data": {
@@ -25,7 +29,7 @@ async function GetData() {
             "datasets":[{"label":"Gauge Height",
             "data": values,
             "fill":false,
-            "borderColor":"rgb(75, 192, 192)",
+            "borderColor":"#D99441",
             "lineTension":0.1}]},
             "options":{
                 responsive: false,
@@ -56,4 +60,167 @@ async function GetData() {
             }
         }
     );
-}
+    
+    
+    /* Site 1 */
+    var dates = [];
+    var values = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[1].values[0].value.length
+    for (i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[1].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
+    }
+    var sitename = msg.value.timeSeries[1].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[1].variable.variableDescription
+    
+    var ctx = document.getElementById("chartjs-1");
+    
+    var myChart = new Chart(ctx, {
+        "type":"line",
+        "data": {
+            "labels": dates,
+            "datasets":[{"label":"Gauge Height",
+            "data": values,
+            "fill":false,
+            "borderColor":"#D99441",
+            "lineTension":0.1}]},
+            "options":{
+                responsive: false,
+                maintainAspectRatio: true,
+                title: {
+                    display: true,
+                    text: sitename + ' - PID: ' + sitecode,
+                    fontSize : 18
+                },
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                             displayFormats: {
+                                day: 'MMM D'
+                            }
+                        }
+                    }],
+    
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: siteDescription
+                        }
+                    }]
+    
+                }
+            }
+        }
+    );
+    
+    /* Site 2 */
+    var dates = [];
+    var values = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[2].values[0].value.length
+    for (i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[2].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
+    }
+    var sitename = msg.value.timeSeries[2].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[2].variable.variableDescription
+    
+    var ctx = document.getElementById("chartjs-2");
+    
+    var myChart = new Chart(ctx, {
+        "type":"line",
+        "data": {
+            "labels": dates,
+            "datasets":[{"label":"Gauge Height",
+            "data": values,
+            "fill":false,
+            "borderColor":"#D99441",
+            "lineTension":0.1}]},
+            "options":{
+                responsive: false,
+                maintainAspectRatio: true,
+                title: {
+                    display: true,
+                    text: sitename + ' - PID: ' + sitecode,
+                    fontSize : 18
+                },
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                             displayFormats: {
+                                day: 'MMM D'
+                            }
+                        }
+                    }],
+    
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: siteDescription
+                        }
+                    }]
+    
+                }
+            }
+        }
+    );
+    
+    /* Site 3 */
+    var dates = [];
+    var values = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[3].values[0].value.length
+    for (i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[3].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
+    }
+    var sitename = msg.value.timeSeries[3].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[3].variable.variableDescription
+    
+    var ctx = document.getElementById("chartjs-3");
+    
+    var myChart = new Chart(ctx, {
+        "type":"line",
+        "data": {
+            "labels": dates,
+            "datasets":[{"label":"Gauge Height",
+            "data": values,
+            "fill":false,
+    "borderColor":"#D99441",
+            "lineTension":0.1}]},
+            "options":{
+                responsive: false,
+                maintainAspectRatio: true,
+                title: {
+                    display: true,
+                    text: sitename + ' - PID: ' + sitecode,
+                    fontSize : 18
+                },
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                             displayFormats: {
+                                day: 'MMM D'
+                            }
+                        }
+                    }],
+    
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: siteDescription
+                        }
+                    }]
+    
+                }
+            }
+        }
+    );
+    }
