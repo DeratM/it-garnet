e75227fff1078749741ba15cc4dbc546067ef64b
DeratM <143150184+DeratM@users.noreply.github.com>
2024-04-23 16:00:50 +0000

    water level v2

---
+++ water-services/assests/javascript/scriptsV2.js
@@ -0,0 +1,59 @@
+async function GetData() {  
+    var myURL = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=07055680,07055646,07055780,07055660&indent=on&period=P7D&siteStatus=all&parameterCd=00065";
+    var msgObject = await fetch(myURL);
+    var msgJSONText = await msgObject.text();
+    var msg = JSON.parse(msgJSONText);  
+    
+
+    var dates = [];
+    var values = [];
+    /* fLen contains the length of the array (number of values) */
+    fLen = msg.value.timeSeries[0].values[0].value.length
+    for (var i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[0].values[0].value[i].value
+        dates[i] = msg.value.timeSeries[0].values[0].value[i].dateTime
+    }
+    var sitename = msg.value.timeSeries[0].sourceInfo.siteName
+    var sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value
+    var siteDescription = msg.value.timeSeries[0].variable.variableDescription
+
+    let ctx = document.getElementById("chartjs-0");
+    var myChart = new Chart(ctx, {
+        "type":"line",
+        "data": {
+            "labels": dates,
+            "datasets":[{"label":"Gauge Height",
+            "data": values,
+            "fill":false,
+            "borderColor":"rgb(75, 192, 192)",
+            "lineTension":0.1}]},
+            "options":{
+                responsive: false,
+                maintainAspectRatio: true,
+                title: {
+                    display: true,
+                    text: sitename + ' - PID: ' + sitecode,
+                    fontSize : 18
+                },
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                             displayFormats: {
+                                day: 'MMM D'
+                            }
+                        }
+                    }],
+    
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: siteDescription
+                        }
+                    }]
+    
+                }
+            }
+        }
+    );
+}
